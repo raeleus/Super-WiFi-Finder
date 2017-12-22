d50d9aa58df33c4e7d32481d63339cdd35972684
Raymond Buckley <raymond.ray3k@gmail.com>
2017-12-21 17:41:10 -0800

    Added base code from VaultBound.

--- desktop/src/com/ray3k/superwififinder/desktop/DesktopLauncher.java
+++ desktop/src/com/ray3k/superwififinder/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.ray3k.superwififinder.Core;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                config.width = 800;
+                config.height = 600;
 		new LwjglApplication(new Core(), config);
 	}
 }
